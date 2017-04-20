db6af8a068379b324828104ab28de707adf50f23
Ivan HA <hangolam@gmail.com>
2017-04-20 22:31:06 +0800

    add video list and video player

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,1 +1,27 @@
 import React from 'react';
+
+const VideoDetail = ({video}) => {
+    // To protect if video data is not yet available
+    if (!video) {
+        return <div>Loading...</div>;
+    }
+
+    const videoTitle = video.snippet.title;
+    const videoDescription = video.snippet.description;
+    const videoId = video.id.videoId;
+    const url = `https://www.youtube.com/embed/${videoId}`;
+
+    return (
+        <div className='video-detail col-md-8'>
+            <div className='embed-responsive embed-responsive-16by9'>
+                <iframe className='embed-responsive-item' src={url}></iframe>
+            </div>
+            <div className='details'>
+                <div>{videoTitle}</div>
+                <div>{videoDescription}</div>
+            </div>
+        </div>
+    );
+};
+
+export default VideoDetail;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
+const API_KEY = 'AIzaSyCa2rcS8Or53INN_K8RkdoC4ngHDnJqzTs'
 
 // Create component that produce HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {videos: []};
+
+        YTSearch({key: API_KEY, term: 'angular'}, (videos) => {
+            this.setState({videos});
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoDetail video={this.state.videos[0]} />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
 }
 
 // Take a component and put it in the DOM
