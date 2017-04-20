102ae4ba450f24fa84c79e9172df4498aec56c4a
Ivan HA <hangolam@gmail.com>
2017-04-20 21:28:23 +0800

    init search bar component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,1 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
+
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {term: 'react tutorial'};
+    }
+
+    render() {
+        return (
+            <div>
+                <input
+                    value={this.state.term}
+                    onChange={event => this.setState({term: event.target.value})} />
+            </div>
+        );
+    }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Create component that produce HTML
+const App = () => {
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
+}
+
+// Take a component and put it in the DOM
+ReactDOM.render(<App />, document.querySelector('.container'));
