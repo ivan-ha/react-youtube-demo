043d5b884bb9ff270446cfcce8f1c0b3f1acadf5
Ivan HA <hangolam@gmail.com>
2017-04-22 00:47:34 +0800

    add select video in video list

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import VideoListItem from './video_list_item';
 
-const VideoList = ({videos}) => {
-    const videoItems = videos.map((video) => {
+const VideoList = (props) => {
+    const videoItems = props.videos.map((video) => {
         return (
-            <VideoListItem key={video.etag} video={video} />
+            <VideoListItem
+                onVideoSelect={props.onVideoSelect}
+                key={video.etag}
+                video={video} />
         );
     });
 
